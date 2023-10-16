f1f01a89d2cacc6b3ff9788aefbb59546cee4f02
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-15 20:47:54 -0400

    Exercise 4.3

--- workshop/exercise-4.3.js
+++ workshop/exercise-4.3.js
@@ -2,7 +2,31 @@ const request = require('request-promise');
 
 const getGeekJoke = async () => {
   // ha hahahh
+    try {
+    const options = {
+      uri: 'https://v2.jokeapi.dev/joke/Programming',
+      qs: {
+        blacklistFlags: 'nsfw,religious,political,racist,sexist',
+        type: 'single,twopart',
+        format: 'json',
+      },
+    };
+
+    const response = await request(options);
+    const joke = JSON.parse(response);
+
+    if (joke.type === 'single') {
+      return joke.joke;
+    } else if (joke.type === 'twopart') {
+      return `${joke.setup} ${joke.delivery}`;
+    } else {
+      throw new Error('Unexpected joke type');
+    }
+  } catch (error) {
+    console.error('Error: ', error);
+    throw error;
+  }
 };
 
 // 4.2
-// getGeekJoke().then((data) => console.log(data));
+getGeekJoke().then((data) => console.log(data));
