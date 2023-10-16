451ba58c9e33b045eb986dee9b29b5e7cfe39fee
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-15 20:22:44 -0400

    Exercise 3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -4,9 +4,23 @@
 const doublesLater = (num) => {
   // 1. waits 2 seconds (You could console a message here. It might make the 2 second wait easier ;)
   // 2. returns the double of the num
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(num * 2);
+    }, 2000);
+  });
 };
 
 // 3. handleSum function (async/await)
+const handleSum = async (num) => {
+  let theSum = 0;
+
+  theSum += await doublesLater(num);
+  theSum += await doublesLater(theSum);
+  theSum += await doublesLater(theSum);
+
+  return theSum;
+};
 
 // 4. verification
 handleSum(10).then((ans) => console.log(ans));
