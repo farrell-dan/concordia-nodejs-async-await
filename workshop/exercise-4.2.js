ff27f6cf714beef1c27563b3e2b79dd22aad1b06
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-15 20:44:19 -0400

    Exercise 4.2

--- workshop/exercise-4.2.js
+++ workshop/exercise-4.2.js
@@ -2,7 +2,31 @@ const request = require('request-promise');
 
 const getPun = async () => {
   // write write write
+  try {
+    const options = {
+      uri: 'https://v2.jokeapi.dev/joke/Pun',
+      qs: {
+        blacklistFlags: 'nsfw,religious,political,racist,sexist',
+        type: 'single', // You can choose 'single' or 'twopart' here
+        format: 'json',
+      },
+    };
+
+    const response = await request(options);
+    const joke = JSON.parse(response);
+
+    if (joke.type === 'single') {
+      return joke.joke;
+    } else if (joke.type === 'twopart') {
+      return `${joke.setup} ${joke.delivery}`;
+    } else {
+      throw new Error('Unexpected joke type');
+    }
+  } catch (error) {
+    console.error('Error: ', error);
+    throw error;
+  }
 };
 
-// 4.2
-// getPun().then((data) => console.log(data));
+4.2
+getPun().then((data) => console.log(data));
