93a3e7acdc046476e0d781ad4582078ff83aeea4
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-15 20:32:54 -0400

    Exercise 4.1

--- workshop/exercise-4.1.js
+++ workshop/exercise-4.1.js
@@ -1,8 +1,22 @@
+
+
 const request = require("request-promise");
 
 const getDadJoke = async () => {
+    try {
+        const random = {
+          uri: "https://icanhazdadjoke.com/",
+          headers: { Accept: "application/json" },
+        };
+    const response = await request (random);
+    const joke = JSON.parse(response);
+    return joke.joke;
+} catch (error){
+    console.error("Error: ", error);
+    throw error;
+}
     // ha hahahh
 };
 
 // 4.1
-// getDadJoke().then((data) => console.log(data));
+getDadJoke().then((data) => console.log(data));
